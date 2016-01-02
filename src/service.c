94b8188b1dc9d46f81a2c3337fbde39d9db3de9f
charles kerr <charlesk@canonical.com>
2016-01-01 20:14:14 -0600

    update copyright dates on changed files (again)

--- src/service.c
+++ src/service.c
@@ -1,9 +1,5 @@
 /*
- * Copyright 2013 Canonical Ltd.
- *
- * Authors:
- *   Charles Kerr <charles.kerr@canonical.com>
- *   Ted Gould <ted@canonical.com>
+ * Copyright 2013-2016 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3, as published
@@ -16,6 +12,10 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ *   Ted Gould <ted@canonical.com>
  */
 
 #include <glib/gi18n.h>
